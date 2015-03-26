eaec9ff5441d5ead4db6d44b0b0578f02ddf45e4
Aaron Dancer <me@aarondancer.com>
2015-03-26 01:31:50 -0500

    Better profile picture sizing

--- js/main.js
+++ js/main.js
@@ -104,14 +104,7 @@ $(window).resize(function(){
 		var currentFontSize = title.css("font-size");
 		title.css("font-size", (parseFloat(currentFontSize) + 1) + "px");
 	}
-	$("[data-match-height]").each(function() {
-		var parentRow = $(this),
-			childrenCols = $(this).find("[data-height-watch]");
-		childrenCols.css('max-height', '');
-		var childHeights = childrenCols.map(function(){ return $(this).height(); }).get(),
-			tallestChild = Math.min.apply(Math, childHeights);
-		childrenCols.css('max-height', tallestChild);
-	});
+	$(".me").css("height", $(".info").height());
 	$("[data-match-width").each(function() {
 		var parentRow = $(this),
 			childrenCols = $(this).find("[data-width-watch]");
